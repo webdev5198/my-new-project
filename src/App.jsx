5de710c68601029fcba323b685f742b3187350a9
webdev5198 <tanzim615@gmail.com>
2023-11-06 17:19:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+      <h1 className = 'text-7xl bg-blue-500'>Vite + React</h1>
+   
+
+     
+  
+ 
+     </>
+  )
+     
+
+   
+}
+
+export default App
